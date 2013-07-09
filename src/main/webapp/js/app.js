7bac0a8bb72ba8d4453125f5f8a448335a17979e
derkoe <christian.koeberl@gmail.com>
2013-07-09 08:32:22 +0200

    step-1

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,1 +1,7 @@
 var todo = angular.module('todo', []);
+
+todo.controller('TodoCtrl', function($scope, $http) {
+  $http.get('data/todo/list').success(function(data) {
+    $scope.todos = data;
+  });
+});
