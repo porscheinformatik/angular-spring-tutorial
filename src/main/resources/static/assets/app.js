6966fa197a2ca59738a356a54a7eb38cbb2f40dc
derkoe <christian.koeberl@gmail.com>
2014-12-17 07:43:08 +0100

    Fixed logout link handling

--- src/main/resources/static/assets/app.js
+++ src/main/resources/static/assets/app.js
@@ -51,7 +51,8 @@
     return {
       restrict : 'A',
       link : function link(scope, element) {
-        element.bind('click', function() {
+        element.bind('click', function(event) {
+          event.preventDefault();
           document.getElementById('logout').submit();
         });
       }
