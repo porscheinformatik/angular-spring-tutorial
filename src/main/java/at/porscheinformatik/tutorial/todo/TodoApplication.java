cfdbf2bf62a146af55afc5ea7a5cf545962bff36
derkoe <christian.koeberl@gmail.com>
2015-02-26 11:36:53 +0100

    Changed versions of JS/CSS libs, versions added to templates via filtering

--- src/main/java/at/porscheinformatik/tutorial/todo/TodoApplication.java
+++ src/main/java/at/porscheinformatik/tutorial/todo/TodoApplication.java
@@ -1,36 +1,15 @@
 package at.porscheinformatik.tutorial.todo;
 
-import antlr.collections.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
-import com.mangofactory.swagger.plugin.EnableSwagger;
-import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
-import com.wordnik.swagger.model.ApiInfo;
-
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@EnableSwagger
+@SpringBootApplication
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class TodoApplication extends SpringBootServletInitializer
 {
-    @Autowired
-    private SpringSwaggerConfig springSwaggerConfig;
-
-    @Autowired
-    private Environment env;
-
     public static void main(String[] args)
     {
         SpringApplication.run(TodoApplication.class, args);
@@ -40,15 +19,5 @@ public class TodoApplication extends SpringBootServletInitializer
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
     {
         return application.sources(TodoApplication.class);
-    }
-
-    @Bean
-    public SwaggerSpringMvcPlugin customImplementation()
-    {
-        return new SwaggerSpringMvcPlugin(springSwaggerConfig)
-            .apiInfo(new ApiInfo("", "", "", "", "", ""))
-            .apiVersion(env.getProperty("info.app.version"))
-            .directModelSubstitute(Iterable.class, List.class)
-            .includePatterns("/api.*");
     }
 }
